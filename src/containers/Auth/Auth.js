e791e3ecc9374e345b96c54d1c06947a7350b2d9
AnastasiaGer <aagermanova@gmail.com>
2021-02-23 14:45:39 -0800

    76

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,36 @@ import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+state = {
+  formControls: {
+    email: {
+      value: '',
+      type: 'email',
+      label: 'Email',
+      errorMessage: 'Need email',
+      valid: false,
+      touched: false,
+      validation: {
+        required: true,
+        email: true
+      }
+    },
+    password: {
+      value: '',
+      type: 'password',
+      label: 'Password',
+      errorMessage: 'Need password',
+      valid: false,
+      touched: false,
+      validation: {
+        required: true,
+        minLength: 6
+      }
+    }
+  }
+}
+
+
   loginHandler = () => {
 
   }
@@ -16,14 +46,38 @@ export default class Auth extends Component {
     event.preventDefault();
   }
 
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}: `, event.target.value)
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
+
   render() {
     return (
       <div className={classes.Auth}>
         <div>
-          <h1>Auth</h1>
+          <h1>Авторизация</h1>
+
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <Input label="Email"/>
-            <Input  label="Password"/>
+
+            { this.renderInputs() }
 
             <Button type="success" 
             onClick={this.loginHandler}>Log in</Button>
